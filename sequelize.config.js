0194dc7912d39b334abd84c82167ea87676fb91d
Ruth Mira <ruth.silva@1risjc.com.br>
2024-03-14 08:39:38 -0300

    teste para sincronizar ao server

--- sequelize.config.js
+++ sequelize.config.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'mssql',
     host: 'mssql', 
-    port: 1433, // A porta padrão do SQL Server
-    username: 'SA', // O usuário SA padrão
-    password: 'abc123.,', // A senha configurada para o usuário SA
-    database: 'PINI' // O nome do banco de dados que você deseja acessar
+    port: 1433, 
+    username: 'SA', 
+    password: 'abc123.,', 
+    database: 'PINI' 
 });
 
 module.exports = sequelize;
